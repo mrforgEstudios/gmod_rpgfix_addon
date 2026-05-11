e191ed3212a556f17ee4e61df07d470419de9253
mrforgEstudios <134918828+mrforgEstudios@users.noreply.github.com>
2026-05-11 12:47:46 +0300

    fix: исправил подключение локализации + обновил README

--- lua/autorun/missile_shootdown.lua
+++ lua/autorun/missile_shootdown.lua
@@ -1,3 +1,6 @@
--- Обновлённый missile_shootdown.lua с использованием локализации
+-- missile_shootdown.lua
+include("sh_localization.lua")
+
 print(RPGFix_L["loaded"])
--- ... (остальной код с L["key"])
+
+-- остальной код...
